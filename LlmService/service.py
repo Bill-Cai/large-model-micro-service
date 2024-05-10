995044008f42f1817366f2202405ef74d697ee20
Bill_Cai <qingmu_0722@163.com>
2024-05-10 15:42:51 +0800

    多个大模型一个后端启动(tinyllama, llama2, mistral)

--- LlmService/service.py
+++ LlmService/service.py
@@ -5,21 +5,37 @@ from LlmService.Model.model import load_model
 from LlmService.router.chat import router as chat_router
 from LlmService.util import stream_out
 
-pipe = None
 verbose = True
+pipe_list = {}
+TINYLLAMA_MODEL_PATH = "/data/qm/huggingface/TinyLlama-1.1B-Chat-v1.0"
+LLAMA2_MODEL_PATH = "/data/qm/huggingface/Llama-2-7b-chat-hf"
+MISTRAL_MODEL_PATH = "/data/qm/huggingface/Mistral-7B-Instruct-v0.2/Mistral-7B-Instruct-v0.2"
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    global verbose, pipe_list, TINYLLAMA_MODEL_PATH, LLAMA2_MODEL_PATH, MISTRAL_MODEL_PATH
     stream_out("\n====> [INFO] lifespan start <====\n", verbose=verbose)
-    global pipe
-    pipe = load_model(
+    pipe_list["TinyLlama-1.1B-Chat-v1.0"] = load_model(
+        model=TINYLLAMA_MODEL_PATH,
+        model_id="TinyLlama-1.1B-Chat-v1.0",
         task="text-generation",
-        model="/home/qm/TinyLlama-1.1B-Chat-v1.0",
-        torch_dtype=torch.bfloat16,
         device_map="auto",
+        torch_dtype=torch.bfloat16,
         verbose=verbose
     )  # load large model
+    pipe_list["Llama-2-7b-chat-hf"] = load_model(
+        model=LLAMA2_MODEL_PATH,
+        model_id="Llama-2-7b-chat-hf",
+        device_map="auto",
+        verbose=verbose
+    )
+    pipe_list["Mistral-7B-Instruct-v0.2"] = load_model(
+        model=MISTRAL_MODEL_PATH,
+        model_id="Mistral-7B-Instruct-v0.2",
+        device_map="auto",
+        verbose=verbose
+    )
     stream_out("\n====> [INFO] model loaded <====\n", verbose=verbose)
     print()
     yield
@@ -36,5 +52,5 @@ app.include_router(
 
 @app.get("/test")
 async def test():
-    stream_out(pipe, verbose=True)
+    stream_out(pipe_list, verbose=True)
     return {"message": "success", "router": "/test"}
